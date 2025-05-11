1fc482e2be165279b21064776625c11d7334a180
Suhani Sahu <sahusuhani14@gmail.com>
2025-05-11 15:27:33 +0530

    added ui and implement logic

--- src/app/[hash]/page.tsx
+++ src/app/[hash]/page.tsx
@@ -1,18 +1,23 @@
+// app/[hash]/page.tsx
+
 import { redirect, notFound } from "next/navigation";
 import { connectToDB } from "@/lib/db";
 import Url, { IUrl } from "@/models/Url";
 
-interface Props {
-  params: { hash: string };
+interface RedirectPageProps {
+  params: {
+    hash: string;
+  };
 }
 
-export default async function RedirectPage({ params }: Props) {
+export default async function RedirectPage({ params }: RedirectPageProps) {
   await connectToDB();
 
   const url = await Url.findOne({ hash: params.hash }).lean<IUrl>();
+  
   if (!url) {
-    notFound();
+    notFound(); // 404 error page
   }
 
-  redirect(url.longUrl);
+  redirect(url.longUrl); // navigate to original URL
 }
